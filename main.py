d5429faa8655d6f6c46a2945357a54ef5232ca55
Jackson Luna <jackluna2007@hotmail.com>
2024-06-18 12:58:06 -0500

    Minor fix update

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from io import BytesIO
 from dotenv import load_dotenv
 from os import getenv
 from fastapi.middleware.cors import CORSMiddleware
-from mangum import Mangum
 
 from helpers.clear_up import Paths, get_Data, get_elements, get_english_level, get_technologies
 from helpers.createListObj import create_list_objects
