a4356d6b283694e72b9dd3c4c036799754d8561a
Jackson Luna <jackluna2007@hotmail.com>
2024-06-25 10:23:23 -0500

    Minor fix update

--- main.py
+++ main.py
@@ -8,7 +8,13 @@ from dotenv import load_dotenv
 from os import getenv
 from fastapi.middleware.cors import CORSMiddleware
 
-from helpers.clear_up import Paths, get_Data, get_elements, get_english_level, get_technologies
+from helpers.clear_up import (
+    Paths,
+    get_Data,
+    get_elements,
+    get_english_level,
+    get_technologies,
+)
 from helpers.createListObj import create_list_objects
 from helpers.get_cities import get_cities_api
 from models.cotization import Cotization
@@ -17,37 +23,31 @@ app = FastAPI()
 
 load_dotenv()
 
-origin = {
-    'http://localhost:5173',
-    'https://dev.d1m5iwwmoe1hji.amplifyapp.com'
-}
+origin = {"http://localhost:5173", "https://dev.d1m5iwwmoe1hji.amplifyapp.com"}
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origin,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 
 @app.post("/cotization")
 def get_current_info(currentData: Cotization):
     try:
+        print(currentData)
         df = get_Data(Paths.DATA)
-
-        df["Aspiración Salarial"] = df["Aspiración Salarial"].str.replace("$", "")
-        df["Aspiración Salarial"] = df["Aspiración Salarial"].str.replace(",", "")
-        df["Aspiración Salarial"] = df["Aspiración Salarial"].str.replace(".", "")
-        df["Aspiración Salarial"] = pd.to_numeric(df["Aspiración Salarial"], errors="coerce")
-        
+        print(df["Aspiración Salarial"])
+      
         position = currentData.position
         technology = currentData.technology
         city = currentData.city
         country = currentData.country
         english_level = currentData.english_level
         years_experience = currentData.years_experience
-        
+
         if years_experience == 0:
             experience_start = 1
             experience_end = 2
@@ -57,41 +57,36 @@ def get_current_info(currentData: Cotization):
         elif years_experience == 2:
             experience_start = 5
             experience_end = None
-        
+
         print(currentData)
-     
+
         if experience_end != None:
-            salary = df[(df["Position"] == position) & 
-                    (df["Lenguaje Principal"] == technology) & 
-                    (df["Country Location of Consultant"] == country) &
-                    (df["Ciudad"] == city) & 
-                    (df["English Proficiency"] == english_level) & 
-                    (df["Años de Experiencia"] >= float(experience_start))& 
-                    (df["Años de Experiencia"] <= float(experience_end))]["Pretension Salarial"].median()
+            salary = df[
+                (df["Position"] == position)
+                & (df["Lenguaje Principal"] == technology)
+                & (df["Country Location of Consultant"] == country)
+                & (df["Ciudad"] == city)
+                & (df["English Proficiency"] == english_level)
+                & (df["Años de Experiencia"] >= float(experience_start))
+                & (df["Años de Experiencia"] <= float(experience_end))
+            ]["Aspiración Salarial"].median()
         else:
-            salary = df[(df["Position"] == position) & 
-                    (df["Nombre Lenguaje Principal"] == technology) & 
-                    (df["Country Location of Consultant"] == country) &
-                    (df["City Location of Consultant"] == city) &
-                    (df["English Proficiency"] == english_level) & 
-                    (df["Experience"] >= float(experience_start))]["Pretension Salarial "].median()
-        
-        if(not(math.isnan(salary))):
-        
+            salary = df[
+                (df["Position"] == position)
+                & (df["Nombre Lenguaje Principal"] == technology)
+                & (df["Country Location of Consultant"] == country)
+                & (df["City Location of Consultant"] == city)
+                & (df["English Proficiency"] == english_level)
+                & (df["Años de Experiencia"] >= float(experience_start))
+            ]["Aspiración Salarial"].median()
+
+        if not (math.isnan(salary)):
+
             print(salary)
-            return JSONResponse(
-                content={
-                    "salary": salary,
-                    "found":True
-                }
-            )
+            return JSONResponse(content={"salary": salary, "found": True})
         else:
-            return JSONResponse(
-                content={
-                    "salary": 0,
-                    "found":False
-                }
-            )
+            print(salary)
+            return JSONResponse(content={"salary": 0, "found": False})
     except requests.exceptions.HTTPError as errh:
         raise HTTPException(status_code=500, detail=f"HTTP error: {errh}")
     except requests.exceptions.ConnectionError as errc:
@@ -101,8 +96,10 @@ def get_current_info(currentData: Cotization):
     except requests.exceptions.RequestException as err:
         raise HTTPException(status_code=500, detail=f"Unexpected error: {err}")
     except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Error processing the Excel file: {e}")
-    
+        raise HTTPException(
+            status_code=400, detail=f"Error processing the Excel file: {e}"
+        )
+
 
 @app.get("/replica")
 async def get_info_total():
@@ -120,27 +117,33 @@ async def get_info_total():
         years_experience = get_elements(dfMATRIZ, "Years Experience")
         finalExperienceList = create_list_objects(years_experience)
 
-        countries = get_elements(dfDATA, "Country Location of Consultant", isCapitalize=True)
+        countries = get_elements(
+            dfDATA, "Country Location of Consultant", isCapitalize=True
+        )
 
         citiesByCountry = []
         for i in range(len(countries)):
             country = countries[i]
-            countries[i]= dfDATA[dfDATA["Country Location of Consultant"] == countries[i]]["Ciudad"].fillna("No city").unique().tolist()
-            temp = {
-                country: countries[i]
-            }
+            countries[i] = (
+                dfDATA[dfDATA["Country Location of Consultant"] == countries[i]][
+                    "Ciudad"
+                ]
+                .fillna("No city")
+                .unique()
+                .tolist()
+            )
+            temp = {country: countries[i]}
             citiesByCountry.append(temp)
 
-
         return JSONResponse(
-          status_code=200,
-          content={
-            "positions": positions,
-            "technologies": technologies,
-            "ubication": citiesByCountry,
-            "english_level": finalEnglishLevelList,
-            "years_experience": finalExperienceList,
-          }
+            status_code=200,
+            content={
+                "positions": positions,
+                "technologies": technologies,
+                "ubication": citiesByCountry,
+                "english_level": finalEnglishLevelList,
+                "years_experience": finalExperienceList,
+            },
         )
 
     except requests.exceptions.HTTPError as errh:
@@ -152,4 +155,6 @@ async def get_info_total():
     except requests.exceptions.RequestException as err:
         raise HTTPException(status_code=500, detail=f"Unexpected error: {err}")
     except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Error processing the Excel file: {e}")
+        raise HTTPException(
+            status_code=400, detail=f"Error processing the Excel file: {e}"
+        )
--- models/cotization.py
+++ models/cotization.py
@@ -4,8 +4,10 @@ from pydantic import BaseModel
 class Cotization(BaseModel):
     position: str
     technology: str
-    city: str
-    country: str
     english_level: str
     years_experience: int
+    city: str
+    country: str
+    
+    
     
